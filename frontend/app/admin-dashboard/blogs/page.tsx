87fd35df5b03e2f51f78341d15619b80460c1ede
Syed-rayyan12 <srayyan314@gmail.com>
2026-09-16 22:41:10 +0500

    code updated of blog post type

--- frontend/app/admin-dashboard/blogs/page.tsx
+++ frontend/app/admin-dashboard/blogs/page.tsx
@@ -1,8 +1,5 @@
 import AdminBlogsClient from "./admin-blogs-client";
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
-
 export default function AdminBlogsPage() {
   return <AdminBlogsClient />;
 }
